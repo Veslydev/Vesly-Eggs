c5cb1c184625d7cd3a5395018d054a25fadfb321
Veslydev <74131882+Veslydev@users.noreply.github.com>
2023-10-24 17:29:20 +0300

    .
    .

--- Paper-Optimized/install.sh
+++ Paper-Optimized/install.sh
@@ -8,41 +8,41 @@ if [ -f "${SERVER_JARFILE}" ]; then
 else
     PROJECT=paper
     if [ -n "${DL_PATH}" ]; then
-        echo "Using supplied download url: ${DL_PATH}"
+        echo "Verilen indirme URL'sini kullanma: ${DL_PATH}"
         DOWNLOAD_URL=$(eval echo $(echo ${DL_PATH} | sed -e 's/{{/${/g' -e 's/}}/}/g'))
     else
         VER_EXISTS=$(curl -s https://api.papermc.io/v2/projects/${PROJECT} | jq -r --arg VERSION $MINECRAFT_VERSION '.versions[] | contains($VERSION)' | grep -m1 true)
         LATEST_VERSION=$(curl -s https://api.papermc.io/v2/projects/${PROJECT} | jq -r '.versions' | jq -r '.[-1]')
         if [ "${VER_EXISTS}" = "true" ]; then
-            echo "Version is valid. Using version ${MINECRAFT_VERSION}"
+            echo "Version geçerlidir. Sürüm kullanma ${MINECRAFT_VERSION}"
         else
-            echo "Specified version not found. Defaulting to the latest ${PROJECT} version"
+            echo "Belirtilen sürüm bulunamadı. Varsayılan olarak en son ${PROJECT} sürümü"
             MINECRAFT_VERSION=${LATEST_VERSION}
         fi
         BUILD_EXISTS=$(curl -s https://api.papermc.io/v2/projects/${PROJECT}/versions/${MINECRAFT_VERSION} | jq -r --arg BUILD ${BUILD_NUMBER} '.builds[] | tostring | contains($BUILD)' | grep -m1 true)
         LATEST_BUILD=$(curl -s https://api.papermc.io/v2/projects/${PROJECT}/versions/${MINECRAFT_VERSION} | jq -r '.builds' | jq -r '.[-1]')
         if [ "${BUILD_EXISTS}" = "true" ]; then
-            echo "Build is valid for version ${MINECRAFT_VERSION}. Using build ${BUILD_NUMBER}"
+            echo "Derleme ${MINECRAFT_VERSION} sürümü için geçerlidir. Yapı ${BUILD_NUMBER} kullanılıyor"
         else
-            echo "Using the latest ${PROJECT} build for version ${MINECRAFT_VERSION}"
+            echo "MINECRAFT_VERSION} sürümü için en son ${PROJECT} derlemesini kullanma"
             BUILD_NUMBER=${LATEST_BUILD}
         fi
         JAR_NAME=${PROJECT}-${MINECRAFT_VERSION}-${BUILD_NUMBER}.jar
-        echo "Version being downloaded"
-        echo "MC Version: ${MINECRAFT_VERSION}"
-        echo "Build: ${BUILD_NUMBER}"
-        echo "JAR Name of Build: ${JAR_NAME}"
+        echo "İndirilmekte olan sürüm"
+        echo "MC Versiyonu: ${MINECRAFT_VERSION}"
+        echo "Yapı: ${BUILD_NUMBER}"
+        echo "JAR Yapının Adı: ${JAR_NAME}"
         DOWNLOAD_URL=https://api.papermc.io/v2/projects/${PROJECT}/versions/${MINECRAFT_VERSION}/builds/${BUILD_NUMBER}/downloads/${JAR_NAME}
     fi
     cd /mnt/server || exit
     mkdir logs
-    echo "Running curl -o ${SERVER_JARFILE} ${DOWNLOAD_URL}"
+    echo "curl -o çalıştırılıyor ${SERVER_JARFILE} ${DOWNLOAD_URL}"
     if [ -f "${SERVER_JARFILE}" ]; then
         mv "${SERVER_JARFILE}" "${SERVER_JARFILE}.old"
     fi
     curl -o "${SERVER_JARFILE}" "${DOWNLOAD_URL}"
     if [ ! -f server.properties ]; then
-        echo "Downloading MC server.properties"
+        echo "MC server.properties dosyasını indirme"
         curl -o server.properties https://raw.githubusercontent.com/parkervcp/eggs/master/minecraft/java/server.properties
         curl -o spigot.yml https://raw.githubusercontent.com/Veslydev/Vesly-Eggs/main/Paper-Optimized/config/spigot.yml
         curl -o bukkit.yml https://raw.githubusercontent.com/Veslydev/Vesly-Eggs/main/Paper-Optimized/config/bukkit.yml
